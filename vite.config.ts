63c8d92190dd57926114410478e7062a433181ae
AntonPetrenko <109439896+mantr88@users.noreply.github.com>
2024-01-22 16:45:22 +0200

    add: base url

--- vite.config.ts
+++ vite.config.ts
@@ -6,7 +6,7 @@ export default ({ mode }: { mode: string }) => {
 
   return defineConfig({
     // To access env vars here use process.env.VITE_API_KEY
-
+    base: "/feedback-log/",
     define: {
       "process.env.VITE_API_KEY": JSON.stringify(process.env.VITE_API_KEY),
     },
